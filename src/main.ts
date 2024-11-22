e67396a4805cddbb646374530a0ffee38929c547
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-11-22 01:21:35 -0300

    feat(styles) Add scss and convert style.css -> styles/main.scss :sparkles:

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./styles/main.scss";
+import typescriptLogo from "./typescript.svg";
+import sassLogo from "./sass.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${typescriptLogo}" class="logo ts" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <a href="https://sass-lang.com/" target="_blank">
+      <img src="${sassLogo}" class="logo sass" alt="Sass logo" />
+    </a>
+    <h1>Vite + TypeScript + Sass</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -19,6 +23,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
